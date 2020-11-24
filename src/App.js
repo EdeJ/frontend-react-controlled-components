ad9c26c9c71afc0f4f236d6414257c9f67a8c9a8
Emiel de Jong <emieldejong@xs4all.nl>
2020-11-24 12:09:30 +0100

    form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [formValues, setFormValues] = useState({
+    name: '',
+    age: 0
+  });
+
   return (
     <div>
-      Maak hier jouw formulier!
+      <form>
+        <label htmlFor="name">
+          <input name="name" type="text" value={formValues.name} />
+        </label>
+        <label htmlFor="age">
+          <input name="age" type="text" value={formValues.age} />
+        </label>
+      </form>
     </div>
   );
 }
