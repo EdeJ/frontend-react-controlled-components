ecf30e307ee070b719b431e2ae0414541e7b32ed
Emiel de Jong <emieldejong@xs4all.nl>
2020-11-24 13:36:49 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -8,15 +8,64 @@ function App() {
     age: 0
   });
 
+  function onChangeHandler(event) {
+    console.log(event.target.name + ' = ' + event.target.value);
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value
+    });
+  }
+
+  function onSubmitHandler(event) {
+    console.log(formValues);
+    event.preventDefault();
+  }
+
   return (
     <div>
-      <form>
-        <label htmlFor="name">
-          <input name="name" type="text" value={formValues.name} />
-        </label>
-        <label htmlFor="age">
-          <input name="age" type="text" value={formValues.age} />
-        </label>
+      <form onSubmit={onSubmitHandler} >
+        <fieldset>
+          <legend>Gegevens</legend>
+          <label>Naam:
+            <input
+              name="name"
+              type="text"
+              value={formValues.name}
+              onChange={onChangeHandler}
+            />
+          </label>
+          <label>Leeftijd:
+            <input
+              name="age"
+              type="text"
+              value={formValues.age}
+              onChange={onChangeHandler}
+            />
+          </label>
+        </fieldset>
+        <fieldset>
+          <legend>Jouw review</legend>
+          <label>Hoe heb je dit recept gevonden?
+            <select
+              name="source"
+              value={formValues.source}
+              onChange={onChangeHandler}
+            >
+              <option value="google">Google</option>
+              <option value="friend">vriend</option>
+              <option value="advertisement">Advertentie</option>
+              <option value="nonOfAbove">Anders</option>
+            </select>
+          </label>
+          <label>Opmerkingen:
+            <textarea
+              name="comments"
+              value={formValues.comments}
+              onChange={onChangeHandler}
+            />
+          </label>
+        </fieldset>
+        <button type="submit">Versturen</button>
       </form>
     </div>
   );
